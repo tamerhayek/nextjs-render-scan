d1f6fae66675509de027648935d5ab70a622d308
tamerhayek <dev@tamerhayek.com>
2025-04-14 15:41:22 +0200

    fix: responsiveness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 			<ReactScan />
 			<body className={`${geistSans.variable} ${geistMono.variable}`}>
 				{children}
